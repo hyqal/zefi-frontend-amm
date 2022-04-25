97acdfdcd853708c83b4b6140fd0293f20b59353
Hyqal AZF <ehyqal@gmail.com>
2022-04-25 14:57:48 +0800

    sdk

--- src/components/swap/ConfirmSwapModal.tsx
+++ src/components/swap/ConfirmSwapModal.tsx
@@ -1,4 +1,4 @@
-import { currencyEquals, Trade } from '@pancakeswap-libs/sdk'
+import { currencyEquals, Trade } from '@ehyqal/mafia-sdk'
 import React, { useCallback, useMemo } from 'react'
 import TransactionConfirmationModal, {
   ConfirmationModalContent,
--- src/utils/maxAmountSpend.ts
+++ src/utils/maxAmountSpend.ts
@@ -1,4 +1,4 @@
-import { CurrencyAmount, ETHER, JSBI } from '@pancakeswap-libs/sdk'
+import { CurrencyAmount, ETHER, JSBI } from '@ehyqal/mafia-sdk'
 import { MIN_ETH } from '../constants'
 
 /**
